a268f74ddf244d9616fff01feb6ce6cedc6b8975
Pritesh Gudge <priteshgudge@gmail.com>
2020-07-06 18:00:34 +0530

    Basic config setup

--- truffle.js
+++ truffle.js
@@ -8,12 +8,12 @@ module.exports = {
         return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
       },
       network_id: '*',
-      gas: 9999999
+      gas: 1345332
     }
   },
   compilers: {
     solc: {
-      version: "^0.4.24"
+      version: "^0.4.25"
     }
   }
 };
